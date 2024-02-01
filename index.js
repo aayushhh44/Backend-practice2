6e8d60565929942c4e1ec681be5f2f59c4958ea2
Aayush Poudel <aayushpoudel59@gmail.com>
2024-02-01 09:33:38 +0545

    change in usermodel and videomodel

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
-const express  = require("express");
-const {connectDB} = require('./src/db/config')
-const app =express();
-connectDB();
+require("dotenv").config();
+const express = require("express");
+const { connectDB } = require("./src/db/config");
+const app = express();
 
-const PORT = process.env.PORT
-
-app.listen(PORT, () =>{
-    console.log('Hi! Its me Aayush')
-})
+const PORT = process.env.PORT;
+connectDB().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection failed ${err}`);
+  });
